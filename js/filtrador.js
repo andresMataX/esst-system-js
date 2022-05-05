bc50872802dc3d6448a36387d94c865ff7d9d3f9
Andrés Mata <andres_mata21@outlook.com>
2022-05-04 23:49:21 -0500

    Creación de opciones al datalist

--- js/filtrador.js
+++ js/filtrador.js
@@ -2,8 +2,14 @@
 const divOpciones = document.querySelector('.opciones');
 const temporalidad = document.querySelector('#temporalidad');
 
+let i = 0;
+
 // Evento
 divOpciones.addEventListener('click', (e) => {
     console.log(e.target.outerText);
-    console.log(temporalidad);
+    let opcionNueva = `
+        <option class="op" value="${i}">
+    `
+    i++;
+    temporalidad.innerHTML += opcionNueva;
 });
