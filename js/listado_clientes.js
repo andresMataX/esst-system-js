94f8db47e7f863847cba43920a4aceb6d440c978
Andrés Mata <andres_mata21@outlook.com>
2022-05-10 00:34:00 -0500

    Obtención y renderización de clientes: listado_clientes.js

--- js/listado_clientes.js
+++ js/listado_clientes.js
@@ -1,10 +1,25 @@
 // URL de clientes
 const urlClientes = 'http://127.0.0.1:5000/read/clientes';
 
+// Referencias HTML
+const tbody = document.querySelector('tbody');
+
 const getClientes = async () => {
     const resp = await fetch(urlClientes);
     const { clientes } = await resp.json();
-    console.log(clientes);
+    for (const cliente of clientes) {
+        tbody.innerHTML += createRow(cliente);
+    }
+}
+
+const createRow = ({ id, name_cli, l_name_cli, cut_name }) => {
+    const html = `<tr>
+                        <th>${id}</th>
+                        <td>${name_cli}</td>
+                        <td>${l_name_cli}</td>
+                        <td>${cut_name}</td>
+                </tr>`
+    return html;
 }
 
 getClientes();
