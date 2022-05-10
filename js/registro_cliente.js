a5ec0da1f71717db0e5f4fa38a1fdcb206471aa7
Andrés Mata <andres_mata21@outlook.com>
2022-05-09 23:40:14 -0500

    Llenado de opciones en tipo de corte con base de datos: registro_cliente.js

--- js/registro_cliente.js
+++ js/registro_cliente.js
@@ -2,13 +2,29 @@
     'use strict'
 
     // Ruta para hacer el registro del cliente
-    const url = 'http://127.0.0.1:5000/create/venta';
+    const urlClientes = 'http://127.0.0.1:5000/create/venta';
+    const urlCortes = 'http://127.0.0.1:5000/read/cortes';
 
     // Referencias HTML
     const aceptar = document.querySelector('#aceptar');
+    const inputCorte = document.querySelector('#cortes');
+
+    // Función para obtener los tipos de corte y agregarlos al datalist
+    const getCortes = async () => {
+        const resp = await fetch(urlCortes);
+        const { cortes } = await resp.json();
+        // Obtención de todos los nombres de los tipos de cortes
+        let opcion = '';
+        for (const { cut_name } of cortes) {
+            opcion += `<option value="${cut_name}" class="opcion">`;
+        }
+        inputCorte.innerHTML += opcion;
+    }
+
+    getCortes();
 
     const register = async (cliente) => {
-        const resp = await fetch(url, {
+        const resp = await fetch(urlClientes, {
             method: 'POST',
             body: JSON.stringify(cliente),
             headers: {
@@ -31,13 +47,14 @@
     const obtenerDatos = () => {
         const { value: nombre } = document.querySelector('#nombre');
         const { value: apellido } = document.querySelector('#apellido');
-        const { index } = document.querySelector('.opcion');
+        const data = document.querySelectorAll('.opcion');
         const cliente = {
             name_cli: nombre,
             l_name_cli: apellido,
-            id_cut_type: index + 1
+            id_cut_type: data
         }
-        register(cliente);
+        console.log(cliente);
+        // register(cliente);
     }
 
     aceptar.addEventListener('click', () => {
